73bc8793abd40540b9fb504387a0042bcc19b130
Ezra Sowden-Guzman <ezratic@gmail.com>
2024-02-07 19:14:00 -0500

    fix loading spinner colors

--- components/LoadingSpinner.vue
+++ components/LoadingSpinner.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="m-auto flex justify-center">
         <div
-            class="h-5 w-5 animate-spin rounded-full border-[4px] border-l-stormPurple border-r-stormPurple border-t-stormPurple"
+            class="h-5 w-5 animate-spin rounded-full border-[4px] border-b-white border-l-stormPurple border-r-stormPurple border-t-stormPurple"
         ></div>
     </div>
 </template>
